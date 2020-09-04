f69bd9b7dc3d49571678c27d06952caba1654509
bouziane-94 <matys.bouziane.sio@gmail.com>
2020-09-04 16:30:46 +0200

    Méthodes Repas,Client

--- Repas.java
+++ Repas.java
@@ -1,14 +1,22 @@
 import java.time.LocalDate ;
 public class Repas {
 	
-	int numero ;
+	private int numero ;
 	
-	Localdate date;
+	private Localdate date;
 	
 	public Repas(int numero , Localdate date){
 		super();
 		this.numero = numero;
 		this.date = date ;
 	}
-
+	
+	public getNumero() {
+		return numero;
+		
+	}
+	
+	public void setNumero(int numero){
+		
+	}
 }
